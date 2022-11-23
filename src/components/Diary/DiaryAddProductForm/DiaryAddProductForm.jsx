acf2583835cc2c67162e02cdda4df7286af5f246
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-23 16:52:51 +0200

    Add adaptive

--- src/components/Diary/DiaryAddProductForm/DiaryAddProductForm.jsx
+++ src/components/Diary/DiaryAddProductForm/DiaryAddProductForm.jsx
@@ -6,9 +6,10 @@ const DiaryAddProductForm = () => {
     <form onSubmit={null} className={scss.form}>
       <div className={scss.box}>
         <input type="text" placeholder='Enter product name' className={scss.input} />
-        <input type="text" placeholder='Grams' className={scss.input} />
+        <input type="text" placeholder='Grams' className={`${scss.input} ${scss.grams}`} />
       </div>
-      <button type='submit' className={scss.button}>Add product</button>
+      <button type='submit' className={scss.buttonAdd}>Add</button>
+      <button type='submit' className={scss.buttonPlus}></button>
     </form>
   );
 }
