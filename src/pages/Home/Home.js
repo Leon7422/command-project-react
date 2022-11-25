9e7d272895f11beb342f11dc4c47afa20223a7b8
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-11-25 15:36:54 +0100

    Додав анімацію

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,27 +3,30 @@ import { Outlet } from 'react-router-dom';
 
 import { AppBar } from 'components/AppBar/AppBar';
 import DailyCaloriesForm from 'components/DailyCaloriesForm/DailyCaloriesForm';
-import Modal from "components/Modal/Modal";
+import Modal from 'components/Modal/Modal';
 
 import css from './Home.module.scss';
+import Animation from 'components/Animation/Animation';
 
 export default function Home() {
   let [isModalOpened, setIsModalOpened] = useState(false);
 
   const toggleModal = () => {
     setIsModalOpened(!isModalOpened);
-  }
+  };
 
   return (
     <section className={`${css['home__section']}`}>
       <div className={`container ${css['home__container']}`}>
         <AppBar />
         <div className={css.content}>
-          <DailyCaloriesForm openModal={toggleModal}/>
+          <DailyCaloriesForm openModal={toggleModal} />
           <Outlet />
         </div>
-        <Modal toggleModal={toggleModal} isOpen={isModalOpened}/>
+        <Modal toggleModal={toggleModal} isOpen={isModalOpened} />
+
+        <Animation />
       </div>
     </section>
-  )
+  );
 }
