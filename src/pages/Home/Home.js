97017d262dccb3f529cb02d4a6597259560744b3
Leonid Holub <rsleonid@gmail.com>
2022-11-25 18:02:09 +0200

    Додає можливість відображати компонент summary

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 import Default from 'components/Summary/Default/Default';
 import Summary from 'components/Summary/Summary';
 import { AppBar } from 'components/AppBar/AppBar';
 import Modal from 'components/Modal/Modal';
-import selectors from 'redux/selectors';
+// import selectors from 'redux/selectors';
 import css from './Home.module.scss';
 
 export default function Home() {
   let [isModalOpened, setIsModalOpened] = useState(false);
 
-  const isLoggedIn = useSelector(selectors.getIsLoggedIn);
+  // const isLoggedIn = useSelector(selectors.getIsLoggedIn);
+
+  const isLoggedIn = true;
 
   const toggleModal = () => {
     setIsModalOpened(!isModalOpened);
