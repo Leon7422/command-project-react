2bae1b88175e14cb9dd986634e59318c92eab60c
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-27 16:35:41 +0200

    Fix styles

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -15,6 +15,7 @@ export default function Calculator() {
           <DiaryDateCalendar />
           <DiaryAddProductForm />
           <DiaryProductList />
+          <button type='button' onClick={() => {}} className={styles.buttonPlus}></button>
         </div>
         <Summary />
       </div>
