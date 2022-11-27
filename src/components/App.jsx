8df164502fb615e067d19c5badb12390a4792fc8
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-11-27 02:48:26 +0200

    Add public/ private routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import operations from 'redux/operations';
 import selectors from 'redux/selectors';
+import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Diary = lazy(() => import('../pages/Diary/Diary'));
@@ -24,11 +26,24 @@ export const App = () => {
         <Routes>
           <Route path="/" element={<Home />}>
             <Route index element={<Diary />} />
-            <Route path="/diary" element={<Diary />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/calculator" element={<Calculator />} />
+            <Route path="/diary" element={<PrivateRoute redirectTo='/login'><Diary/></PrivateRoute>} />
+            <Route path="/login"
+              element={<PublicRoute
+                Component={<Login />}
+                restricted
+                redirectTo='/diary' />
+            } />
+            <Route path="/register"
+              element={<PublicRoute
+              Component={<Register />}
+                restricted
+                redirectTo='/diary' />} />
+            <Route path="/calculator" element={<PrivateRoute
+              redirectTo='/login'>
+              <Calculator />
+            </PrivateRoute>} />
           </Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
     )
