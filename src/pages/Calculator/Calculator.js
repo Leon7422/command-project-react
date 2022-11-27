c3abea71ecb25351458dbfab52be49710c586101
Leonid Holub <rsleonid@gmail.com>
2022-11-27 12:22:10 +0200

    Додає запит userInfo при перезавантаженні сторінки.

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -8,14 +8,16 @@ import styles from './Calculator.module.scss';
 
 export default function Calculator() {
   return (
-    <div className={styles.section}>
-      <div className={styles.calendar}>
-        <UserNavMob />
-        <DiaryDateCalendar />
-        <DiaryAddProductForm />
-        <DiaryProductList />
+    <section className={styles.section}>
+      <div className={`${styles['calculator__container']}`}>
+        <div className={styles.calendar}>
+          <UserNavMob />
+          <DiaryDateCalendar />
+          <DiaryAddProductForm />
+          <DiaryProductList />
+        </div>
+        <Summary />
       </div>
-      <Summary />
-    </div>
+    </section>
   );
 }
