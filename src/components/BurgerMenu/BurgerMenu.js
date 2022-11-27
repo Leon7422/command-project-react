48daf07c37751d6251a4c2a02c990a5a3aebeecc
Leonid Holub <rsleonid@gmail.com>
2022-11-27 08:05:24 +0200

    Update BurgerMenu.js

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -5,7 +5,7 @@ import css from './BurgerMenu.module.scss';
 export function BurgerMenu() {
   return (
     <div className={css.burger}>
-      <Menu noOverlay width={768}>
+      <Menu noOverlay width={'100%'}>
         <div className={css.links}>
           <Link to="/diary" className={css.link} id="react-burger-cross-btn">
             DIARY
