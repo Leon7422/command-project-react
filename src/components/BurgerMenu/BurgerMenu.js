9decde3881a8fa169d89c0da19920b2e55c4a1f4
Leonid Holub <rsleonid@gmail.com>
2022-11-27 08:13:17 +0200

    Додає іконку BurgerMenu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,22 +1,27 @@
 import { slide as Menu } from 'react-burger-menu';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import css from './BurgerMenu.module.scss';
+import burgerImg from '../../images/baseline-menu-24px.png';
 
 export function BurgerMenu() {
   return (
     <div className={css.burger}>
-      <Menu noOverlay width={'100%'}>
-        <div className={css.links}>
-          <Link to="/diary" className={css.link} id="react-burger-cross-btn">
+      <Menu
+        noOverlay
+        width={'100%'}
+        customBurgerIcon={<img src={burgerImg} alt="three lines" />}
+      >
+        <div className={`burgerLink ${css.links}`}>
+          <NavLink to="/diary" className={css.link} id="react-burger-cross-btn">
             DIARY
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/calculator"
             className={css.link}
             id="react-burger-cross-btn"
           >
             CALCULATOR
-          </Link>
+          </NavLink>
         </div>
       </Menu>
     </div>
