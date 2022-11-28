46059b676efc3122174fa51c8ead4412d5103486
Leonid Holub <rsleonid@gmail.com>
2022-11-28 21:07:04 +0200

    Видаля нікнейм та кнопку logout з модалки.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,12 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import selectors from 'redux/selectors';
-import operations from 'redux/operations';
 import styles from './Modal.module.scss';
 
 const Modal = ({ toggleModal, isOpen, children }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   const onBackdropClick = ev => {
     if (ev.currentTarget === ev.target) {
       toggleModal();
     }
   };
-
-  const userName = useSelector(selectors.getUserName);
 
   useEffect(() => {
     const onKeydown = ev => {
@@ -35,14 +26,6 @@ const Modal = ({ toggleModal, isOpen, children }) => {
   if (isOpen) {
     flag.splice(1, 1);
   }
-  const userLogout = () => {
-    dispatch(
-      operations.logOut({
-        username: { name: null, email: null, password: null },
-      })
-    );
-    navigate('/login');
-  };
 
   return (
     <>
@@ -54,20 +37,6 @@ const Modal = ({ toggleModal, isOpen, children }) => {
               className={styles.btnClose}
               onClick={toggleModal}
             ></button>
-            {children ? (
-              <div className={`${styles.bar}`}>
-                <p className={styles.paragraph}>{userName}</p>
-                <button
-                  type="button"
-                  className={styles.button}
-                  onClick={userLogout}
-                >
-                  Exit
-                </button>
-              </div>
-            ) : (
-              ''
-            )}
           </div>
           {children}
         </div>
